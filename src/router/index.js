5cc15c47b4b3a5f0251d2ed61aa91028d115f6fd
Kira Kawai <asymptote.k.k@gmail.com>
2021-02-06 00:58:20 +0900

    Axios導入

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Axios from "../views/Axios.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/axios",
+    name: "axios",
+    component: Axios
   },
   {
     path: "/about",
---
+++ src/views/Axios.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button @click="sendGet">get</button>
+    <button @click="sendPost">post</button>
+    <button @click="sendPut">put</button>
+    <button @click="sendDelete">delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Axios"
+};
+</script>
