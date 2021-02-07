8bd0b0a110576084068a9541ed638fd53a0dee07
Kira Kawai <asymptote.k.k@gmail.com>
2021-02-07 20:33:55 +0900

    update pathが入れ子になるバグを修正 任意の都市を表示できるように

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div>都市名を入力</div>
+    <input type="text" v-model="cityName" />
+    <button @click="getCityInfo">表示する</button>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -13,6 +16,12 @@ export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  methods: {
+    getCityInfo() {
+      const cityName = this.cityName;
+      this.$router.push("/city/" + cityName);
+    }
   }
 };
 </script>
